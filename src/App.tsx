45d6e0afd2f8919b35e41f074d72928db2c28025
chintansheth2001 <chintansheth2001@gmail.com>
2023-08-08 12:44:49 +0530

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,6 @@ function App() {
 
   return (
     <>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
