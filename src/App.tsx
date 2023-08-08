40b75275b84ebd29b4ec321b3b233eeeb1a2c1ba
chintansheth2001 <chintansheth2001@gmail.com>
2023-08-08 08:08:23 +0530

    Remove heading

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ function App() {
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
